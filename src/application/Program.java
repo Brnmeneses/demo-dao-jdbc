337e9990adb7ab84b93a969a6b1d2a14745eb0f4
Bruno Meneses <bruno.meneses-ats@parceiros-kroton.com.br>
2019-02-03 21:45:57 -0200

    Delete Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entitites.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();  
 		
@@ -36,6 +39,22 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println(newSeller);
+
+		System.out.println("\n=== Teste 5: seller update  ===");
+		System.out.println("antes....");
+		System.out.println(seller);
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("....depois");
+		System.out.println(seller);
+	
+		System.out.println("\n=== Teste 6: seller delete  ===");
+		System.out.println("Entre Id:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Seller Deleted");
+		sc.close();
 	}
 
 }
